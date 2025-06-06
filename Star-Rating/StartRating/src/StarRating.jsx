be71669379368c47e4e35310c4a759b62b4c073b
nalina <naleenameshram85@gmail.com>
2025-06-06 14:49:37 +0530

    done

--- Star-Rating/StartRating/src/StarRating.jsx
+++ Star-Rating/StartRating/src/StarRating.jsx
@@ -1,11 +1,41 @@
-import React from "react";
-import { CiStar } from "react-icons/ci";
-const StarRating = ({ noOfStar = 5 }) => {
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./App.css";
+
+export default function StarRating({ noOfStars = 5 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    setHover(getCurrentIndex);
+  }
+
+  function handleMouseLeave() {
+    setHover(rating);
+  }
+
   return (
-    <div>
-      <CiStar />
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+
+        return (
+          <div className="starts">
+            <FaStar
+              key={index}
+              className={index <= (hover || rating) ? "active" : "inactive"}
+              onClick={() => handleClick(index)}
+              onMouseMove={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeave()}
+              size={40}
+            />
+          </div>
+        );
+      })}
     </div>
   );
-};
-
-export default StarRating;
+}
