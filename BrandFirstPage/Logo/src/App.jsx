4a568b68c0f07aaed0984212250a53e77365c14f
nalina <naleenameshram85@gmail.com>
2025-06-13 03:34:21 +0530

    done

---
+++ BrandFirstPage/Logo/src/App.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+import HeroSection from "./HeroSection";
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ BrandFirstPage/Logo/src/Navigation.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+import logo from "./assets/Screenshot 2025-06-12 141245.png";
+import Brandlogo from "./assets/Screenshot 2025-06-12 141535.png";
+const Navigation = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src={logo} alt="Refresh" />
+      </div>
+      <div className="menu">
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+          <li href="#">Feedback</li>
+          <li href="#">Help</li>
+        </ul>
+      </div>
+
+      <button>login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
