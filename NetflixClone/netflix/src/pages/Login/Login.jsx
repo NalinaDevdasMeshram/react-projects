43af12c61d1f8470d40fb3098de87833948a3e7f
nalina <naleenameshram85@gmail.com>
2025-07-27 00:16:24 +0530

    workingon

--- NetflixClone/netflix/src/pages/Login/Login.jsx
+++ NetflixClone/netflix/src/pages/Login/Login.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 const Login = () => {
+  const [signState, setSignState] = useState("Sign In");
   return (
     <div className="login">
       <img src={logo} alt="" className="login-logo" />
       <div className="login_form">
         <h2>Sign Up</h2>
         <form>
-          <input type="text" placeholder="Enter Your Name" required />
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Enter Your Name" required />
+          ) : (
+            <></>
+          )}
           <input type="email" placeholder="Enter Youe Email" required />
           <input type="password" placeholder="Enter Password" required />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
               <label htmlFor="">Remember Me</label>
             </div>
+            <p>Need help?</p>
           </div>
-          <p>Need help?</p>
         </form>
+        <div className="switch_login">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix ?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
+            </p>
+          ) : (
+            <p>
+              Already have Account ?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign In Now
+              </span>
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
